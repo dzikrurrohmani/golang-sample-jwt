c561381e5aaac37a819877c380449f098d0047a1
dzikrur rohmani <dzikrurrohmanizrmh@gmail.com>
2022-07-09 11:09:38 +0700

    use bearer toker

--- main.go
+++ main.go
@@ -1,56 +1,129 @@
+// package main
+
+// import (
+// 	"net/http"
+
+// 	"github.com/gin-gonic/gin"
+// )
+
+// type AuthHeader struct {
+// 	AuthorizationHeader string `header:"Authorization"`
+// }
+
+// func main() {
+// 	routerEngine := gin.Default()
+// 	routerEngine.Use(AuthMiddleware())
+// 	routerGroup := routerEngine.Group("/api")
+
+// 	routerGroup.GET("/customer", func(ctx *gin.Context) {
+// 		ctx.JSON(http.StatusOK, gin.H{
+// 			"message": "customer",
+// 		})
+// 	})
+
+// 	routerGroup.GET("/product", func(ctx *gin.Context) {
+// 		ctx.JSON(http.StatusOK, gin.H{
+// 			"message": "product",
+// 		})
+// 	})
+
+// 	err := routerEngine.Run(":8888")
+// 	if err != nil {
+// 		panic(err)
+// 	}
+// }
+
+// func AuthMiddleware() gin.HandlerFunc {
+// 	return func(ctx *gin.Context) {
+// 		h := AuthHeader{}
+// 		if err := ctx.ShouldBindHeader(&h); err != nil {
+// 			ctx.JSON(http.StatusUnauthorized, gin.H{
+// 				"message": err.Error(),
+// 			})
+// 		}
+
+// 		if h.AuthorizationHeader == "123456" {
+// 			ctx.Next()
+// 		} else {
+// 			ctx.JSON(http.StatusUnauthorized, gin.H{
+// 				"message": "token invalid",
+// 			})
+// 			ctx.Abort()
+// 		}
+// 	}
+// }
+
 package main
 
 import (
+	"fmt"
 	"net/http"
+	"strings"
 
 	"github.com/gin-gonic/gin"
+	"github.com/golang-jwt/jwt"
 )
 
 type AuthHeader struct {
 	AuthorizationHeader string `header:"Authorization"`
 }
 
+type Credential struct {
+	Username string `json:"username"`
+	Password string `json:"password"`
+}
+
+var (
+	ApplicationName  = "Enigma"
+	JwtSigningMethod = jwt.SigningMethodHS256
+	JwtSignatureKey  = []byte("3N!GM4")
+)
+
+type MyClaims struct {
+	jwt.StandardClaims
+	Username string `json:"username"`
+	Email    string `json:"email"`
+}
+
 func main() {
 	routerEngine := gin.Default()
+	routerEngine.Use(AuthTokenMiddleware())
 	routerGroup := routerEngine.Group("/api")
 
-	routerGroup.GET("/customer", func(ctx *gin.Context) {
-		authHeader := AuthHeader{}
-		if err := ctx.ShouldBindHeader(&authHeader); err != nil {
-			ctx.JSON(http.StatusUnauthorized, gin.H{
-				"message": "Invalid",
+	routerGroup.POST("/auth/login", func(c *gin.Context) {
+		var user Credential
+		//authHeader := AuthHeader{}
+		if err := c.BindJSON(&user); err != nil {
+			c.JSON(http.StatusUnauthorized, gin.H{
+				"message": "Unauthorized",
 			})
 			return
 		}
 
-		if authHeader.AuthorizationHeader == "123456" {
-			ctx.JSON(http.StatusOK, gin.H{
-				"message": "customer",
+		if user.Username == "enigma" && user.Password == "123" {
+			token, err := GenerateToken(user.Username, "admin@enigmacamp.com")
+			if err != nil {
+				c.AbortWithStatus(401)
+				return
+
+			}
+			c.JSON(200, gin.H{
+				"token": token,
 			})
-			return
+		} else {
+			c.AbortWithStatus(401)
 		}
-		ctx.JSON(http.StatusUnauthorized, gin.H{
-			"message": "Unauthorized",
+	})
+
+	routerGroup.GET("/customer", func(ctx *gin.Context) {
+		ctx.JSON(http.StatusOK, gin.H{
+			"message": "customer",
 		})
 	})
 
 	routerGroup.GET("/product", func(ctx *gin.Context) {
-		authHeader := AuthHeader{}
-		if err := ctx.ShouldBindHeader(&authHeader); err != nil {
-			ctx.JSON(http.StatusUnauthorized, gin.H{
-				"message": "Invalid",
-			})
-			return
-		}
-
-		if authHeader.AuthorizationHeader == "123456" {
-			ctx.JSON(http.StatusOK, gin.H{
-				"message": "product",
-			})
-			return
-		}
-		ctx.JSON(http.StatusUnauthorized, gin.H{
-			"message": "Unauthorized",
+		ctx.JSON(http.StatusOK, gin.H{
+			"message": "product",
 		})
 	})
 
@@ -58,30 +131,95 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
+
+	// err := routerEngine.Run(":8888")
+	// if err != nil {
+	// 	panic(err)
+	// }
+
 }
 
-
-func AuthMiddleware() gin.HandlerFunc {
-	return func(ctx *gin.Context) {
-		if ctx.Request.URL.Path == "/api/auth/login5" {
-			ctx.Next()
+func AuthTokenMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		if c.Request.URL.Path == "/api/auth/login" {
+			c.Next()
+			fmt.Println("sss")
 		} else {
 			h := AuthHeader{}
-			if err := ctx.ShouldBindHeader(&h); err != nil {
-				ctx.JSON(http.StatusUnauthorized, gin.H{
-					"message": err.Error(),
+			if err := c.ShouldBindHeader(&h); err != nil {
+				c.JSON(http.StatusUnauthorized, gin.H{
+					"message": "Unauthrorized",
 				})
+				c.Abort()
+				return
 			}
+			tokenString := strings.Replace(h.AuthorizationHeader, "Bearer ", "", -1)
+			fmt.Println("token", tokenString)
+			if tokenString == "" {
+				c.JSON(http.StatusUnauthorized, gin.H{
+					"message": "Unauthrorized",
+				})
+				c.Abort()
+				return
+			}
+			fmt.Println(1)
 
-			if h.AuthorizationHeader == "ini_token" {
-				ctx.Next()
+			token, err := ParseToken(tokenString)
+			if err != nil {
+				c.JSON(http.StatusUnauthorized, gin.H{
+					"message": "Unauthrorized",
+				})
+				c.Abort()
+				return
+			}
+			fmt.Println("token:", token)
+			if token["iss"] == ApplicationName {
+				c.Next()
 			} else {
-				ctx.JSON(http.StatusUnauthorized, gin.H{
+				c.JSON(http.StatusUnauthorized, gin.H{
 					"message": "token invalid",
 				})
-				ctx.Abort()
+				c.Abort()
 			}
 		}
+
 	}
 }
 
+func GenerateToken(userName string, email string) (string, error) {
+	claims := MyClaims{
+		StandardClaims: jwt.StandardClaims{
+			Issuer: ApplicationName,
+		},
+		Username: userName,
+		Email:    email,
+	}
+	token := jwt.NewWithClaims(
+		JwtSigningMethod,
+		claims,
+	)
+	return token.SignedString(JwtSignatureKey)
+}
+
+func ParseToken(tokenString string) (jwt.MapClaims, error) {
+	token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+		method, ok := token.Method.(*jwt.SigningMethodHMAC)
+		if !ok {
+			return nil, fmt.Errorf("signing method invalid")
+		} else if method != JwtSigningMethod {
+			return nil, fmt.Errorf("signing method invalid")
+		}
+		fmt.Println(ok)
+
+		return JwtSignatureKey, nil
+	})
+	fmt.Println(err)
+
+	claims, ok := token.Claims.(jwt.MapClaims)
+	fmt.Println(ok, token.Valid)
+	if !ok || !token.Valid {
+		return nil, err
+	}
+	fmt.Println(2)
+	return claims, nil
+}
